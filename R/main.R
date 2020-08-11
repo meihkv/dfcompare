de0ef72b9a3a5bd64cbec19498b22847a4e03b09
meihkv <mjvuong@gmail.com>
2020-08-11 14:55:15 -0400

    add data.table

--- R/main.R
+++ R/main.R
@@ -1,6 +1,19 @@
+
+#Test cases
+#No matching columns
+#No rows
+#No matching keys
+
+# print(sprintf("Columns in %s not in %s:",src_name, tgt_name))
+# print(mismatch_colnames(source, target)[1])
+# print(sprintf("Columns in %s not in %s:",tgt_name, src_name))
+# print(mismatch_colnames(source, target)[2])
+#
+# print("Mismatching datatypes:")
+# print(mismatch_datatypes(source, target))
+
 df_compare = function (source, target, keys) {
-  require(dplyr)
-  require(magrittr)
+  require(data.table)
 
   stopifnot(
     is.data.frame(source),
@@ -13,20 +26,12 @@ df_compare = function (source, target, keys) {
     key_exists(target, keys)
   )
 
-  #Test cases
-  #No matching columns
-  #No rows
-  #No matching keys
   print(mismatch_colnames(source, target))
   print(mismatch_datatypes(source, target))
 
-  return("Done")
-}
+  setDT(source)
+  setDT(target)
 
-# print(sprintf("Columns in %s not in %s:",src_name, tgt_name))
-# print(mismatch_colnames(source, target)[1])
-# print(sprintf("Columns in %s not in %s:",tgt_name, src_name))
-# print(mismatch_colnames(source, target)[2])
-#
-# print("Mismatching datatypes:")
-# print(mismatch_datatypes(source, target))
+  merge()
+
+}
