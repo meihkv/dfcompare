a0eba06eea0d71bcc36dd766b3649038d7cadbb0
meihkv <mjvuong@gmail.com>
2020-08-13 11:22:36 -0400

    add keys to mismatching tables in check_equality

--- R/check_equality.R
+++ R/check_equality.R
@@ -5,15 +5,18 @@
 #' prefix "i." + name of the column to be compared with each other.
 #'
 #' @param column_name name of column to be tested
+#' @param keys vector name of columns to be included as reference
 #' @param datatable name of data table containing the columns to be tested
 #' @return data table with only the tested columns and a mismatch column
 
-check_equality = function (column_name, datatable) {
+check_equality = function (column_name, datatable, keys) {
   column_name = c(column_name, paste("i.",column_name,sep=""))
   column1 = column_name[1]
   column2 = column_name[2]
-  dt = cbind(datatable[,..column_name],
+  dt = cbind(datatable[,..keys],datatable[,..column_name],
              mismatch = (datatable[,..column1] != datatable[,..column2])
   )
-  return(dt[dt[[3]],])
+  #find mismathing column index
+  mismatch_column_position = length(keys) + length(column_name) + 1
+  return(dt[dt[[mismatch_column_position]],])
 }
