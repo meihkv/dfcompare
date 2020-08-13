d88f05db50650380e668befdb597efee35593935
meihkv <mjvuong@gmail.com>
2020-08-13 10:25:16 -0400

    add common_colnames documentation

--- R/common_colnames.R
+++ R/common_colnames.R
@@ -1,3 +1,11 @@
+#' Searches for common column names between two data frames
+#'
+#' This is a private function for dfcompare.
+#'
+#' @param source data frame of source
+#' @param target data frame of target
+#' @return a vector of common column names
+
 common_colnames = function(source, target) {
   col_src = colnames(source)
   col_tgt = colnames(target)
