68cb2df4098c6bcf90af05ffd67fae9c6f3e44b6
meihkv <mjvuong@gmail.com>
2020-08-13 10:52:01 -0400

    fix depends issue in description file

--- R/dfcompare.R
+++ R/dfcompare.R
@@ -30,8 +30,8 @@ dfcompare = function (source, target, keys) {
 
   common = common_colnames(source,target)
 
-  source = data.table::as.data.table(source)[,common]
-  target = data.table::as.data.table(target)[,common]
+  source = data.table::as.data.table(source)[,..common]
+  target = data.table::as.data.table(target)[,..common]
 
   #Remove duplicate keys from source and target
   src_no_dupes = source[!(duplicated(data.table::rleidv(source, cols = keys)) |
