9ec807b779f408ff5f176e5bd44833be6399944a
Michael K Vuong <a137235@progressive.com>
2020-08-14 16:14:13 -0400

    improve colnames function to return data frames

--- R/mismatch_colnames.R
+++ R/mismatch_colnames.R
@@ -1,13 +1,19 @@
 mismatch_colnames = function (source, target) {
   #finds columns that are not in common with each other
-  #returns a list of character vectors
+  #returns a list of data frame vectors
+
+  src_name = deparse(substitute(source,env=parent.frame()))
+  tgt_name = deparse(substitute(target,env=parent.frame()))
 
   col_src = colnames(source)
   col_tgt = colnames(target)
 
-  src_not_tgt = col_src[!(col_src %in% col_tgt)]
-  tgt_not_src = col_tgt[!(col_tgt %in% col_src)]
-  list = list(source_not_target = src_not_tgt,
-              target_not_source = tgt_not_src)
-  return(list)
+  src_not_tgt = as.data.frame(col_src[!(col_src %in% col_tgt)])
+  tgt_not_src = as.data.frame(col_tgt[!(col_tgt %in% col_src)])
+
+  names(src_not_tgt)[1] = paste(src_name,"not in",tgt_name,sep=" ")
+  names(tgt_not_src)[1] = paste(tgt_name,"not in",src_name,sep=" ")
+
+  return(list(src_not_tgt, tgt_not_src))
 }
+
