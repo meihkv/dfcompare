23e1b99a40e307ff9c5f263e3aa20998c3df0254
meihkv <mjvuong@gmail.com>
2020-08-15 12:48:20 -0400

    add testcol_exists

--- tests/testthat/testcol_exists.R
+++ tests/testthat/testcol_exists.R
@@ -1,4 +1,14 @@
-#
-# test_that("column exists", {
-#   expect_setequal(col_exists(iris), c('Species'))
-# })
+
+test_that("column exists", {
+  expect_true(col_exists(iris,'Species'))
+})
+
+
+test_that("column doesn't exist", {
+  expect_false(col_exists(iris,'test me'))
+})
+
+
+test_that("column doesn't exist", {
+  expect_false(col_exists(iris,c('test me','Species')))
+})
