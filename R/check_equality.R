5b736d8e092146b355fd03036c0cdf64d946b232
meihkv <mjvuong@gmail.com>
2020-08-15 14:43:23 -0400

    add src name and tgt name to check_equality

--- R/check_equality.R
+++ R/check_equality.R
@@ -9,15 +9,28 @@
 #' @param datatable name of data table containing the columns to be tested
 #' @return data table with only the tested columns and a mismatch column
 
-check_equality = function (column_name, datatable, keys) {
+check_equality = function (column_name, datatable, keys, dt_names) {
 
   src_col_name = column_name
   tgt_col_name = paste("i.",column_name,sep="")
 
+  src_name = dt_names[1]
+  tgt_name = dt_names[2]
+
   sorted_column_names = c(keys, src_col_name, tgt_col_name)
 
-  tested_columns = cbind(datatable[,..sorted_column_names])
+  #Subset columns from data table using sorted_column_names
+  #Subset rows where data table source and target columns do not equal
+  tested_columns = datatable[,..sorted_column_names][
+                          datatable[,..sorted_column_names][[src_col_name]] !=
+                          datatable[,..sorted_column_names][[tgt_col_name]]
+                          ,]
+
+  #Rename tested columns to reflect target and source names
+
+  setnames(tested_columns,src_col_name,src_name)
+  setnames(tested_columns,tgt_col_name,tgt_name)
 
   #Return only mismatches
-  return(tested_columns[tested_columns[[2]]!=tested_columns[[3]],])
+  return(tested_columns)
 }
