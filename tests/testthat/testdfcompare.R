b6ffcf5cbad73957c0d922dcf590cb5a0232379f
meihkv <mjvuong@gmail.com>
2020-08-16 16:24:03 -0400

    add dfcompare test file

---
+++ tests/testthat/testdfcompare.R
@@ -0,0 +1,22 @@
+set.seed(10)
+
+random = function(){
+  result = rnorm(1)
+  if(result<0){
+    return(1)
+  }
+  else{
+    return(result)
+  }
+}
+
+target = data.frame(sapply(longley, function(x) x*replicate(n = nrow(longley), random())))
+
+test_that("returns names", {
+  result = dfcompare(longley, target, 'Year', summary = FALSE)
+  expect_equal(length(result),
+              6)
+  expect_true(all.equal(result,result))
+})
+
+
