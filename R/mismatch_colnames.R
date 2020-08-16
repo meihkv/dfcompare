d1f6787547fbee545bbac6d25a732420623fa113
meihkv <mjvuong@gmail.com>
2020-08-16 16:03:00 -0400

    refactor to support data tables

--- R/mismatch_colnames.R
+++ R/mismatch_colnames.R
@@ -2,26 +2,26 @@
 #'
 #' This is a private function for dfcompare.
 #'
-#' @param source data frame of source
-#' @param target data frame of target
-#' @return a list of data frames containing uncommon column names
+#' @param source data table of source
+#' @param target data table of target
+#' @return a list of data tables containing uncommon column names
 #'
-mismatch_colnames = function (source, target) {
-  #finds columns that are not in common with each other
-  #returns a list of data frame vectors
-
-  src_name = deparse(substitute(source,env=parent.frame()))
-  tgt_name = deparse(substitute(target,env=parent.frame()))
+mismatch_colnames = function (source, target, src_name="source", tgt_name="target") {
 
   col_src = colnames(source)
   col_tgt = colnames(target)
 
-  src_not_tgt = as.data.frame(col_src[!(col_src %in% col_tgt)])
-  tgt_not_src = as.data.frame(col_tgt[!(col_tgt %in% col_src)])
+  src_not_tgt = col_src[!(col_src %in% col_tgt)]
+  tgt_not_src = col_tgt[!(col_tgt %in% col_src)]
 
-  names(src_not_tgt)[1] = paste(src_name,"not in",tgt_name,sep=" ")
-  names(tgt_not_src)[1] = paste(tgt_name,"not in",src_name,sep=" ")
+  uncommon = list(
+    data.table::data.table(src_not_tgt),
+    data.table::data.table(tgt_not_src)
+  )
 
-  return(list(src_not_tgt, tgt_not_src))
+  names(uncommon)[[1]] = paste(src_name,"not in",tgt_name,sep=" ")
+  names(uncommon)[[2]] = paste(tgt_name,"not in",src_name,sep=" ")
+
+  return(uncommon)
 }
 
